9847016797c46ea327c8c61060098a5d1be9fe10
superrafyYT <86516779+superrafyYT@users.noreply.github.com>
2024-10-29 00:12:39 +0100

    added queries

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, User as AngularFireUser } from '@angular/fire/auth';
 import { updateProfile } from 'firebase/auth';
-import { Firestore, getDoc, deleteDoc, doc, limit, setDoc, collection, query, getDocs } from '@angular/fire/firestore';
+import { Firestore, getDoc, deleteDoc, doc, limit, setDoc, collection, query, getDocs, where, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -28,31 +29,15 @@ export class LoginComponent {
       const user: AngularFireUser = userCredential.user;
       this.message = `Login successful! Welcome, ${user.displayName}`;
       console.log('User:', user);
-      await this.deleteCollection('users', user.uid);
+      const data = await this.GetUsersByCondition("age", ">=", 10, 5).subscribe(data => {
+        const users = data;
+        users.forEach(user => {
+          console.log(user.age, user.email);
+        });
+      });
     } catch (error) {
       this.message = 'Login failed. Please check your credentials.';
       console.error('Login error:', error);
-    }
-  }
-
-  async deleteCollection(collectionPath: string, document: string): Promise<void> {
-    try {
-      const collectionRef = collection(this.firestore, collectionPath);
-
-      const limitedQuery = query(collectionRef, limit(1));
-
-      const querySnapshot = await getDocs(limitedQuery);
-      console.log("Got query, lenght of docs is:" + querySnapshot.docs.length)
-      if (!querySnapshot.empty) {
-        const documentToDelete = querySnapshot.docs[0];
-        console.log('Specific field:', documentToDelete.data()['username']);
-        await deleteDoc(doc(this.firestore, collectionPath, documentToDelete.id));
-        console.log(`Document with ID ${documentToDelete.id} deleted successfully.`);
-      } else {
-        console.log('No document found to delete.');
-      }
-    } catch (error) {
-      console.error('Error deleting document:', error);
     }
   }
   //#endregion
@@ -72,19 +57,6 @@ export class LoginComponent {
     }
   }
 
-  async CreateUserDocument(userId: string, username: string, email: string) {
-    try {
-      const userRef = doc(this.firestore, 'users', userId);
-      await setDoc(userRef, {
-        username: username,
-        email: email,
-      });
-      console.log('User document created with ID:', userId);
-    } catch (error) {
-      console.error('Error creating user document:', error);
-    }
-  }
-
   async SetDisplayName(user: AngularFireUser, displayName: string): Promise<void> {
     if (user) {
       try {
@@ -96,6 +68,30 @@ export class LoginComponent {
         this.messageSignUp = 'Failed to set username. Please check your connection';
       }
     }
+  }
+  //#endregion
+
+  //#region WRITE
+  async CreateUserDocument(userId: string, username: string, email: string) {
+    try {
+      const userRef = doc(this.firestore, 'users', userId);
+      await setDoc(userRef, {
+        username: username,
+        email: email,
+        age: Math.floor(Math.random() * 99) + 1
+      });
+      console.log('User document created with ID:', userId);
+    } catch (error) {
+      console.error('Error creating user document:', error);
+    }
+  }
+  //#region 
+
+  //#region QUERIES
+  GetUsersByCondition(field: string, operator: any, value: any, limitNum: any): Observable<any[]> {
+    const usersCollection = collection(this.firestore, 'users');
+    const q = query(usersCollection, where(field, operator, value), limit(limitNum));
+    return collectionData(q);
   }
   //#endregion
 }
